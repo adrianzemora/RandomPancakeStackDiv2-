8565bece11b7b869ddeb6cd8f255e8bc179d5078
adrianzemora <adryano_ze@yahoo.com>
2015-05-01 15:52:07 +0300

    Added empty solution

---
+++ Pancakes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pancakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
